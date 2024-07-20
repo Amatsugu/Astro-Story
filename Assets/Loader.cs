7f7fdb522c973e0fdcbfc292d5b9f66eb955a6ef
Amatsugu <khamraj@kaisei.app>
2024-07-20 19:41:12 -0400

    Added Scene loader

---
+++ Assets/Loader.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+
+#if UNITY_EDITOR
+using UnityEditor.SceneManagement;
+#endif
+
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+[ExecuteInEditMode]
+public class Loader : MonoBehaviour
+{
+	public string[] scenePaths;
+
+	private void Start()
+	{
+		foreach (var path in scenePaths)
+		{
+			var scene = SceneManager.GetSceneByPath(path);
+#if UNITY_EDITOR
+			if (!Application.isPlaying)
+			{
+				if (!scene.isLoaded)
+					EditorSceneManager.OpenScene(path, OpenSceneMode.Additive);
+			}
+#else
+			SceneManager.LoadScene(scene.buildIndex, LoadSceneMode.Additive);
+#endif
+		}
+	}
+}
